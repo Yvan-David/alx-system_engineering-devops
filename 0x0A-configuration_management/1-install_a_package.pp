b3edac978c2bbb8616812c384bd83a33db77c9c2
Yvan-David <irankundayvan2020@gmail.com>
2023-04-23 18:10:35 +0200

    correct task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # installing flask package using pip3
 package { 'flask':
-          ensure   => ['2.1.0', 'installed']
+          ensure   => ['2.1.0', 'installed'],
           provider => 'pip3',
 }
