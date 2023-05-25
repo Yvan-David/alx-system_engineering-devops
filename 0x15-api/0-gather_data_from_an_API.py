eb7a386f2cd2eb511e221604cbb0e6bd31eec41c
Yvan-David <irankundayvan2020@gmail.com>
2023-05-25 13:27:16 +0200

    REST api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" takes in a letter and sends a POST request to
+to use json file of the response """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    url = "https://jsonplaceholder.typicode.com/todos/"
+    r = requests.get(url)
+    total_tasks = 0
+    done_tasks = 0
+    n = int(sys.argv[1])
+    content = []
+
+    try:
+        json_dict = r.json()
+        if json_dict:
+            for i in range(1, json_dict.__len__()):
+                if json_dict[i].get('userId') == n:
+                    total_tasks += 1
+                if json_dict[i].get('userId') == n and \
+                   json_dict[i].get('completed') is True:
+                    done_tasks += 1
+                    content.append(json_dict[i].get('title'))
+            print(f"Employee {json_dict[n].get('username')}", end='')
+            print(f" is done with tasks({done_tasks}/{total_tasks}):")
+            for i in content:
+                print(f"\t{i}")
+        else:
+            print("No result")
+
+    except ValueError:
+        print("Not a valid JSON")
