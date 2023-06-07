6275574d225d85ed98213f6c79ae1513bbbaadf6
Yvan-David <irankundayvan2020@gmail.com>
2023-06-07 23:04:00 +0200

    fix the fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,18 @@
 # resolve server error 500
+$str = "<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+
+<p>My first paragraph.</p>
+
+</body>
+</html>
+        "
 file { '/var/www/html/index.html':
       ensure  => 'file',
-      content => 'Holberton',
+      content => $str,
       owner   => 'root',
       group   => 'root',
       mode    => '0611',
