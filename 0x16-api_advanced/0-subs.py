3b08808183b13d277b6a452a0b2599767a5f6ded
Yvan-David <irankundayvan2020@gmail.com>
2023-06-08 10:42:51 +0200

    subsribers of a subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,11 @@
 #!/usr/bin/python3
-"""Module that access the reddit through api
-
-This is a script that check subreddit's subscribers.
-
-This file can also be imported as a module and contains the following
-function:
-    * number_of_subscribers - returns number of subreddit subscribers
-"""
-
+"""This script returns the number of subscribers for a
+subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Function that returns number of subcribers of a subreddit
-
-    Parameters
-    ----------
-    subreddit : str
-        name of a subreddit
-
-    Returns
-    -------
-    number of subscribers
-        if the subreddit exists
-    """
-
-    header = {'User-Agent': 'reddit/2023.20.0'}
+    """Return the number of subscribers of a subreddit"""
     r = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json',
                      headers={'User-Agent': 'reddit/2023.20.0'})
     if (r.status_code >= 300):
