27b95af0e018dc7f9513d49ee11da4b8c7385ea0
Yvan-David <irankundayvan2020@gmail.com>
2023-06-08 10:46:55 +0200

    subsribers of a subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the number of subscribers of a subreddit"""
-    r = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
+    r = requests.get('https://www.reddit.com/r/{}/about.json'.format(
+        subreddit),
                      headers={'User-Agent': 'reddit/2023.20.0'})
     if (r.status_code >= 300):
         return (0)
